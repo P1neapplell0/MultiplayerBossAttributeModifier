17a26541a6ed062283d881a07bc0d50221263688
p1nero <1070402601@qq.com>
2026-05-14 06:39:17 +0800

    修最大血没跟着动

--- src/main/java/com/p1nero/multiplayer_boss_fight/MultiplayerBossManager.java
+++ src/main/java/com/p1nero/multiplayer_boss_fight/MultiplayerBossManager.java
@@ -144,6 +144,9 @@ public class MultiplayerBossManager {
     }
 
     private static void applyAttributeModifiers(LivingEntity livingEntity, ResolvedBossConfig config, int nearbyPlayers) {
+        double oldMaxHealth = livingEntity.getMaxHealth();
+        double currentHealth = livingEntity.getHealth();
+
         for (ResolvedAttributeModifier modifierConfig : config.attributeModifiers()) {
             AttributeInstance attributeInstance = livingEntity.getAttribute(modifierConfig.attribute());
             if (attributeInstance == null) {
@@ -163,6 +166,14 @@ public class MultiplayerBossManager {
                     modifierConfig.operation()
             );
             attributeInstance.addTransientModifier(modifier);
+        }
+
+        double newMaxHealth = livingEntity.getMaxHealth();
+
+        if (oldMaxHealth > 0) {
+            double ratio = newMaxHealth / oldMaxHealth;
+            double newHealth = currentHealth * ratio;
+            livingEntity.setHealth((float) Math.min(newMaxHealth, newHealth));
         }
 
         if (livingEntity.getHealth() > livingEntity.getMaxHealth()) {
